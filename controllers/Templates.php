6841d20ea2576cece613e2b602f223d2a7f353e0
Mary <mary.lucey@yahoo.com>
2023-08-07 18:40:51 -0500

    Initalize Mustache Engine with partials loader in Templates class constructor

--- controllers/Templates.php
+++ controllers/Templates.php
@@ -5,6 +5,9 @@ class Templates {
 	
 	public function __construct() {
 		$this->m = (new \Mustache_Engine(
+		    [
+			'partials_loader' => new \Mustache_Loader_FilesystemLoader('views/partials'),
+			]
     }
 
 ?>
